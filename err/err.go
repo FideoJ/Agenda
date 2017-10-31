30f253e8c72357c3cd369eddfc9f8811086308e0
MarshallW906 <MaticWang@hotmail.com>
2017-11-01 04:17:33 +0800

    entity/meeting.go, service/meeting.go, storage.go. err.go

--- err/err.go
+++ err/err.go
@@ -3,5 +3,6 @@ package err
 import "errors"
 
 var (
-	UserAlreadyExists error = errors.New("user already exists")
+	UserAlreadyExists         error = errors.New("user already exists")
+	MeetingTitleAlreadyExists error = errors.New("title already exists")
 )
---
+++ service/meeting.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"time"
+
+	"github.com/MarshallW906/Agenda/entity"
+	"github.com/MarshallW906/Agenda/err"
+	"github.com/MarshallW906/Agenda/storage"
+)
+
+var (
+	timeLayoutStr string
+)
+
+func init() {
+	timeLayoutStr = "2006-01-02 15:04:05"
+}
+
+func CreateMt(title string, startTimeStr string, endTimeStr string, sponsor string, participants []string) error {
+	users := storage.LoadUsers()
+	meetings := storage.LoadMeetings()
+	if meetings.Query(title) != nil {
+		return err.MeetingTitleAlreadyExists
+	}
+
+	sTime, terr := time.Parse(timeLayoutStr, startTimeStr)
+	if terr != nil {
+		return terr
+	}
+	eTime, terr := time.Parse(timeLayoutStr, endTimeStr)
+	if terr != nil {
+		return terr
+	}
+	sponsorUsr := users.Query(sponsor)
+
+	meetings.Add(&entity.Meeting{
+		Title:        title,
+		StartTime:    sTime,
+		EndTime:      eTime,
+		Sponsor:      sponsorUsr,
+		Participants: participants,
+	})
+	storage.StoreMeetings(meetings)
+	return nil
+}
